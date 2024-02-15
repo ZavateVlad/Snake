79244dbd400bba0aa1c2ae3ec24d259075798593
Zavate_Vlad <zavatevlad26@gmail.com>
2024-02-15 12:00:21 +0200

    Added file to keep the high score

--- scoreboard.py
+++ scoreboard.py
@@ -7,7 +7,8 @@ class Scoreboard(Turtle):
     def __init__(self):
         super().__init__()
         self.score = 0
-        self.high_score = 0
+        with open('data.txt', mode='r') as file:
+            self.high_score = file.read()
         self.color('white')
         self.penup()
         self.goto(0, 250)
@@ -18,12 +19,14 @@ class Scoreboard(Turtle):
         self.clear()
         self.write(f'Score: {self.score} High Score:{self.high_score}', align=ALIGN, font=FONT)
 
-
     def reset(self):
-        if self.score > self.high_score:
+        if self.score > int(self.high_score):
             self.high_score = self.score
         self.score = 0
+        with open('data.txt', mode='w') as file:
+            file.write(str(self.high_score))
         self.update_scoreboard()
+
     # def game_over(self):
     #     self.goto(0,0)
     #     self.write("Game over.", align=ALIGN, font=FONT)
